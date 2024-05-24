523738cd8328a32738867c6b22547def4169913a
Livanhernandez <livanhernandez9@gmail.com>
2024-05-24 10:14:39 -0400

    Add - Added file with new program added

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log('Welcome to Holberton School, what is your name?');
+
+readline.question('', (name) => {
+  console.log(`Your name is: ${name}`);
+
+  readline.close();
+});
+
+readline.on('close', () => {
+  console.log('This important software is now closing');
+});
