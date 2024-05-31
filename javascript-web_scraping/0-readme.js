7f08298960fb07b01c66bd65b4d421ad5f64a192
Livanhernandez <livanhernandez9@gmail.com>
2024-05-31 11:11:28 -0400

    modified files

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,19 +2,18 @@
 const fs = require('fs');
 
 const filePath = process.argv[2];
+const content = process.argv[3];
 
 if (!filePath) {
   console.error('Error: Please provide a file path as an argument.');
   process.exit(1);
 }
 
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
     // Convert error object to a more informative JSON-like format
     console.error({
       error: err
     });
-  } else {
-    console.log(data);
   }
 });
